a1a2e7af2715a119a98c0daa2b865162b4ea5538
satishjhaofficial <satishjhaofficial@gmail.com>
2022-08-16 23:07:33 +0530

    Fonts 4

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,55 @@ export default function Document() {
     <Html>
       <Head>
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link
+          rel="preload"
+          href="/fonts/futura-book-bt.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/futura-medium-bt.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/futura-heavy-bt.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/bitsumishi.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/myriad-pro/myriadpro-regular.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/myriad-pro/myriadpro-semibold.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/myriad-pro/myriadpro-bold.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
